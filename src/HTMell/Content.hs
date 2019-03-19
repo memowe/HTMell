52981cb0c40bf0c59c91beacf7a7b12c0bcc8665
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2019-03-19 10:40:20 +0100

    Add first node path operations

---
+++ src/HTMell/Content.hs
@@ -0,0 +1,15 @@
+module HTMell.Content where
+
+import System.FilePath
+
+data Node = Node {
+        dir     :: FilePath,
+        path    :: FilePath,
+        home    :: String
+}
+
+relPath :: Node -> FilePath
+relPath (Node d p _) = makeRelative d p
+
+pathParts :: Node -> [FilePath]
+pathParts node = map dropTrailingPathSeparator $ splitPath $ relPath node
