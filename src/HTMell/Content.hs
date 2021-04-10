4dee65fc9be6a004aa6cd4fcaeea0bf78a05d642
Mirko Westermeier <mirko@westermeier.de>
2021-04-10 17:12:47 +0200

    Use String instead of Text for now

--- src/HTMell/Content.hs
+++ src/HTMell/Content.hs
@@ -2,7 +2,7 @@ module HTMell.Content (HTContent(..), html, page) where
 
 import HTMell.Path (HTPath)
 import Data.Map (Map)
-import Data.Text (Text)
+import Data.Text (pack)
 import Data.ByteString.Lazy (ByteString)
 import Cheapskate (markdown, def)
 import Text.Blaze.Html (Html, toHtml)
@@ -11,11 +11,11 @@ import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 data HTContent = HTContent {
     path        :: HTPath,
     metadata    :: Map String String,
-    rawContent  :: Text
+    rawContent  :: String
 }
 
 html :: HTContent -> Html
-html htc = toHtml . markdown def $ rawContent htc
+html htc = toHtml . markdown def . pack $ rawContent htc
 
 page :: HTContent -> ByteString
 page = renderHtml . html
