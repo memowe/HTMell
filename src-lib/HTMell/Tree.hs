5bf472a419b4761dd8e01e049bff0312216d80bf
Mirko Westermeier <mirko@westermeier.de>
2021-07-22 21:09:41 +0200

    Add content to HNodes

--- src-lib/HTMell/Tree.hs
+++ src-lib/HTMell/Tree.hs
@@ -16,19 +16,21 @@ data HNode c = HNode {
     -- | defines an order for all children of its parent
     ord :: Integer,
     -- | All children of this 'HNode', addressed by their relative paths
-    children :: Map String HNode
+    children :: Map String (HNode c),
+    -- | Content of this node
+    content :: c
 } deriving (Eq, Show)
 
 summary :: HNode c -> String
 -- ^ Very short structural summary of a given 'HTree'
-summary (HNode _ children)
+summary (HNode _ children _)
     | null children = ""
     | otherwise     = "(" ++ toStr children ++ ")"
     where
         toStr       = intercalate "," . map pair . assocs
         pair (k, t) = k ++ summary t
 
-findHNode :: HNode -> String -> Maybe HNode
+findHNode :: HNode c -> String -> Maybe (HNode c)
 -- ^ Allows to select a "deep" 'HNode' inside a given tree via a
 --   combined path, separated by slashes. For example, this would
 --   select the @"foo"@ child's child @"bar"@ in a given @tree@:
--- test/HTMell/TestTree.hs
+++ test/HTMell/TestTree.hs
@@ -3,21 +3,22 @@ module HTMell.TestTree ( testTree ) where
 import Test.Tasty ( testGroup )
 import Test.Tasty.HUnit ( testCase, (@?=) )
 import HTMell.Tree ( HNode(..), summary, findHNode )
+import HTMell.Util ( cempty )
 import Data.Map ( empty, fromList )
 import Data.Maybe ( isNothing, fromJust )
 
-trivialTree = HNode 42 empty
-childTree   = HNode 17 (fromList [("foo", HNode 42 empty)])
+trivialTree = HNode 42 empty cempty
+childTree   = HNode 17 (fromList [("foo", HNode 42 empty cempty)]) cempty
 exampleTree = HNode 17 (fromList [
         ("foo", HNode 42 (fromList [
             ("bar", HNode 37 (fromList [
-                ("baz", HNode 108 empty),
-                ("quux", HNode 109 empty)
-            ])),
-            ("bidu", HNode 25 empty)
-        ])),
-        ("xnorfzt", HNode 666 empty)
-    ])
+                ("baz", HNode 108 empty cempty),
+                ("quux", HNode 109 empty cempty)
+            ]) cempty),
+            ("bidu", HNode 25 empty cempty)
+        ]) cempty),
+        ("xnorfzt", HNode 666 empty cempty)
+    ]) cempty
 
 testSummary = testGroup "Tree summary"
     [ testCase "Empty tree" $ summary trivialTree @?= ""
@@ -35,7 +36,7 @@ testFindHNode = testGroup "Find HNodes"
     , testCase "Empty query" $
         findHNode childTree "" @?= Nothing
     , testCase "Direct child" $
-        findHNode childTree "foo" @?= Just (HNode 42 empty)
+        findHNode childTree "foo" @?= Just (HNode 42 empty cempty)
     , testCase "Complex subtree query" $
         findHNode exampleTree "foo/bar" @?=| "(baz,quux)"
     , testCase "Complex leaf query" $
