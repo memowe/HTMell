eea4afda5d054087009c7dafd64b92735bf6490b
Mirko Westermeier <mirko@westermeier.de>
2021-07-29 10:28:17 +0200

    Add forgotten separator comment in Util

--- src-lib/HTMell/Util.hs
+++ src-lib/HTMell/Util.hs
@@ -33,6 +33,8 @@ splitNodePath :: String -> (Integer, String)
 -- prop> splitNodePath "foo" == (0, "foo")
 splitNodePath = fst . head . readP_to_S ordNodePath
 
+-- Pseudo/Empty content instance, useful for testing ---------------------------
+
 -- | Trivial pseudo content: useful for testing
 data PseudoContent = PseudoContent
     deriving (Eq, Show)
