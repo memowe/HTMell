4bc427e4de8402ee8be10c2cb27d1aeebe25d40b
Mirko Westermeier <mirko@westermeier.de>
2021-08-05 10:51:02 +0200

    Add API docs

--- src-lib/HTMell/Content/Markdown.hs
+++ src-lib/HTMell/Content/Markdown.hs
@@ -1,6 +1,17 @@
+{-|
+Module      : HTMell.Content.Markdown
+Description : HTMell markdown content
+Copyright   : (c) 2021 Mirko Westermeier
+License     : MIT
+
+Loading HTMell content from markdown files
+-}
+
 module HTMell.Content.Markdown
   (
+  -- * What is markdown content?
     MarkdownContent(..)
+  -- * Obtaining markdown content
   , readMarkdown
   , loadMarkdownContent
   ) where
@@ -12,6 +23,10 @@ import qualified Data.Text as T
 import Data.Text ( Text )
 import CMark ( commonmarkToHtml )
 
+-- | A markdown content value consists of metadata and markdown data. The
+-- 'metadata' is read as "HTMell.Content.Metadata.Frontmatter", the
+-- markdown is just the source after the frontmatter data and will be
+-- translated to HTML via "CMark" when calling 'toHTML'.
 data MarkdownContent = MarkdownContent
   { meta :: Map String String
   , html :: Text
@@ -22,11 +37,13 @@ instance HTMellContent MarkdownContent where
   metadata    = meta
   toHTML      = html
 
+-- | Build a 'MarkdownContent' value from a source 'String'.
 readMarkdown :: String -> MarkdownContent
 readMarkdown str = MarkdownContent fm html
   where
     (fm, rest)  = splitFrontmatter str
     html        = commonmarkToHtml [] $ T.pack rest
 
+-- | Read a 'MarkdownContent' value from a file.
 loadMarkdownContent :: FilePath -> IO (Maybe MarkdownContent)
 loadMarkdownContent path = Just . readMarkdown <$> readFile path
